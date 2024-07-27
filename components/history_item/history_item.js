92af52c91475dd21577a3bda5f1d1422a48d2913
locolemat <122878505+locolemat@users.noreply.github.com>
2024-07-27 21:32:19 +0800

    Removed redundant history components

--- components/history_item/history_item.js
+++
@@ -1,9 +0,0 @@
-angular.module('tgMiniApp').component('historyItem', {
-    templateUrl: 'components/history_item/history_item.html',
-    controller: function HistoryItemController() {
-      
-    },
-    bindings:{
-      content: '='
-    }
-  });
